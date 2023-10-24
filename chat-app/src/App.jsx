49f31259ec884421bef9cf65678171d2a44ff679
cyro <joshuawang2048@gmail.com>
2023-10-23 18:43:10 -0700

    made self messaging possible

--- chat-app/src/App.jsx
+++ chat-app/src/App.jsx
@@ -3,31 +3,39 @@ import { useState } from "react";
 
 function App() {
     const [message, setMessage] = useState("");
+    const [messages, setMessages] = useState([]);
 
     const handleChange = (e) => {
         if (e.key === "Enter") {
-            sendMessage({ message });
+            sendMessage(message);
             setMessage("");
         } else {
             setMessage(e.target.value);
         }
     };
 
-    return (
-        <input
-            id="textBox"
-            className="textBox"
-            type="text"
-            placeholder="Send a message"
-            value={message}
-            onChange={handleChange}
-            onKeyDown={handleChange}
-        />
-    );
-}
+    const sendMessage = (message) => {
+        setMessages((prevMessages) => [...prevMessages, message]);
+    };
 
-function sendMessage(message) {
-    console.log(message);
+    return (
+        <>
+            {messages.map((msg, index) => (
+                <p key={index} style={{ textAlign: "left" }}>
+                    {msg}
+                </p>
+            ))}
+            <input
+                id="textBox"
+                className="textBox"
+                type="text"
+                placeholder="Send a message"
+                value={message}
+                onChange={handleChange}
+                onKeyDown={handleChange}
+            />
+        </>
+    );
 }
 
 export default App;
