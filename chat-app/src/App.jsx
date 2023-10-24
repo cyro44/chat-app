5d776bc08098e2a3a80bc5ec4993b410317e7d57
cyro <joshuawang2048@gmail.com>
2023-10-24 16:54:54 -0700

    addded usernames

--- chat-app/src/App.jsx
+++ chat-app/src/App.jsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 function App() {
     const [message, setMessage] = useState("");
     const [messages, setMessages] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+    const [username, setUsername] = useState("");
 
     const handleChange = (e) => {
         if (e.key === "Enter") {
@@ -18,11 +20,46 @@ function App() {
         setMessages((prevMessages) => [...prevMessages, message]);
     };
 
+    // const modal = document.querySelector(".usernameModal");
+    // const span = document.getElementsByClassName("close")[0];
+    // span.onclick = function () {
+    //     modal.style.display = "none";
+    // };
+
+    const toggleModal = () => {
+        setShowModal(!showModal);
+    };
+
     return (
         <>
+            <button className="usernameBtn" onClick={toggleModal}>
+                Set or change your username
+            </button>
+            <div
+                className="usernameModal"
+                style={{ display: showModal ? "block" : "none" }}
+            >
+                <span className="close" onClick={toggleModal}>
+                    <i className="fa-solid fa-square-xmark"></i>
+                </span>
+                <h1>Set or Change your Username</h1>
+                <input
+                    className="usernameInput"
+                    type="text"
+                    placeholder="Type in your username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                            localStorage.setItem("username", username);
+                            setUsername("");
+                        }
+                    }}
+                />
+            </div>
             {messages.map((msg, index) => (
                 <p key={index} style={{ textAlign: "left" }}>
-                    {msg}
+                    <strong className="strong">{ localStorage.getItem("username") }</strong> {msg}
                 </p>
             ))}
             <input
